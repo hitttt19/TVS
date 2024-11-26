d5710313dd9c13bc17ff45eea677d1422564d3e8
hitttt19 <hitttt19@gmail.com>
2024-11-26 10:52:48 +0800

    gm

--- forgot_password.php
+++ forgot_password.php
@@ -43,8 +43,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $mail->isSMTP();                                      // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                  // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                              // Enable SMTP authentication
-            $mail->Username   = 'hitttt19@gmail.com';             // Your Gmail address
-            $mail->Password   = 'uouv rcfm fpnt lblg';            // Your Gmail app password (NOT your Gmail account password)
+            $mail->Username   = 'reguladrive@gmail.com';             // Your Gmail address
+            $mail->Password   = 'befx wqgc wdgi oenj';            // Your Gmail app password (NOT your Gmail account password)
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;    // Enable TLS encryption
             $mail->Port       = 587;                               // TCP port to connect to
 
